4fe1da7fff0d43937edb74af28761d3d14ff9f39
Ben Polgardy <benpolgardy@gmail.com>
2019-09-21 13:19:45 +0200

    add functions

--- data_manager.py
+++ data_manager.py
@@ -10,3 +10,34 @@ def get_mentor_names_by_first_name(cursor, first_name):
                    {'first_name': first_name})
     names = cursor.fetchall()
     return names
+
+
+@database_common.connection_handler
+def get_mentor_names(cursor):
+    cursor.execute("""
+                    SELECT first_name, last_name FROM mentors
+                   """)
+    names = cursor.fetchall()
+    return names
+
+
+@database_common.connection_handler
+def get_mentor_nicknames(cursor, city):
+    cursor.execute("""
+                    SELECT nick_name FROM mentors
+                    WHERE city = %(city)s
+                   """,
+                   {'city': city})
+    nicknames = cursor.fetchall()
+    return nicknames
+
+
+@database_common.connection_handler
+def get_applicant_info(cursor, applicant_name):
+    cursor.execute("""
+                    SELECT * FROM applicants
+                    WHERE first_name = %(applicant_name)s
+                   """,
+                   {'applicant_name': applicant_name})
+    applicant_info = cursor.fetchall()
+    return applicant_info
--- main.py
+++ main.py
@@ -9,12 +9,26 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/mentors-with-best-first-name')
+@app.route('/mentor-names')
 def mentor_names():
     # We get back dictionaries here (for details check 'database_common.py')
-    mentor_names = data_manager.get_mentor_names_by_first_name('László')
+    mentor_names = data_manager.get_mentor_names()
 
     return render_template('mentor_names.html', mentor_names=mentor_names)
+
+
+@app.route('/mentor-nicknames')
+def mentor_nicknames():
+    mentor_nicknames = data_manager.get_mentor_nicknames('Miskolc')
+
+    return render_template('mentor_names.html', mentor_names=mentor_nicknames, mentor_nicknames=True)
+
+
+@app.route('/applicant-info')
+def applicant_info():
+    applicant_info = data_manager.get_applicant_info('Carol')
+
+    return render_template('applicant_info.html', applicant_info=applicant_info)
 
 
 if __name__ == '__main__':
